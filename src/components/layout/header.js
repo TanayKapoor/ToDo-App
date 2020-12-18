dd9b341fe233f31f1ec2cbfd856f88b5e67fe269
TanayKapoor <itstkaay@gmail.com>
2020-12-18 20:26:00 +0530

    Polishes - a little bit

---
+++ src/components/layout/header.js
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todolist" />
+        </div>
+      </nav>
+    </header>
+  );
+};

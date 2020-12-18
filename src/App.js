e0cbe2d860e0ec433c58199f3a51d5406cbcc5e4
TanayKapoor <itstkaay@gmail.com>
2020-12-18 21:23:46 +0530

    Modified Sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Header} from './components/layout/header'
+import { Header } from './components/layout/header';
+import { Content } from './components/layout/Content';
 
 export const App = () => (
-    <div className="App">
-      <Header />
-    </div>
+  <div className="App">
+    <Header />
+    <Content />
+  </div>
 );
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
+
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>Next 7 days</li>
+    </ul>
+  </div>
+);

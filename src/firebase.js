62db9aa882100accebb027ede0410d22f42eab35
TanayKapoor <itstkaay@gmail.com>
2020-12-22 15:35:15 +0530

    Connecting to firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig =  firebase.initializeApp({
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    apiID: '',
+    apiKey: '',
+});
+
+export {firebaseConfig as firebase};

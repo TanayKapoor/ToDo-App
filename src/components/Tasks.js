e8f0001fe8d54eb36118b38d2b2031de8a47c0db
tanaykapoor <itstkaay@gmail.com>
2020-12-24 19:44:12 +0530

    Build task component

---
+++ src/components/Tasks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Tasks = () => {
+  const tasks = [];
+
+  let projectName = '';
+
+  return (
+    <div className="tasks" data-testid="tasks">
+      <h2 data-testid="project-name">{projectName}</h2>
+
+      <ul className="tasks-list">
+          {tasks.map(task =>(
+              <li key={`${task.id}`}>
+                  <Checkbox id={task.id} />
+                  <span>{task.task}</span>
+
+              </li>
+          ))}
+
+      </ul>
+    </div>
+  );
+};

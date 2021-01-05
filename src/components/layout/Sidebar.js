a6dcfbc400bb6cb9a4b18a6729d9ed7dd3614e6d
tanay <itstkaay@gmail.com>
2021-01-05 22:46:45 +0530

    Added Context

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar,
 } from 'react-icons/fa';
+import { useSelectedProjectValue } from '../../context';
 
-export const Sidebar = () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-      <li>
-        <span>
-          <FaInbox />
-        </span>
-        <span>Inbox</span>
-      </li>
-      <li>
-        <span>
-          <FaRegCalendar />
-        </span>
-        <span>Today</span>
-      </li>
-      <li>Next 7 days</li>
-    </ul>
+export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, showProject] = useState(true);
 
-    <div className="sidebar__middle">
-      <span><FaChevronDown /></span>
-      <h2>Projects</h2>
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>Next 7 days</li>
+      </ul>
+      <div className="sidebar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__project">Projects will be here</ul>
+      Add Project component Here!!!
     </div>
-    <ul className="sidebar__project">Projects will be here</ul>
-    Add Project component Here!!!
-  </div>
-);
+  );
+};
